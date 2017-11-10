f1b0d56cf294efec3214703e3b369e91804874c8
rbuhian <ronald.buhian0211@gmail.com>
2017-11-10 11:55:44 +0800

    Added Annotation

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,13 +6,28 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Must begin with capital letter and consist of only letters or spaces ")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set;  }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Must begin with capital letter and consist of only letters or spaces ")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
